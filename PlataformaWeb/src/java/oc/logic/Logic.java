227de6021e7adc7cc72b66de00bd37968dc3d5eb
Erickdez <erickhf16@outlook.com>
2019-04-02 18:10:59 -0600

    Packages

---
+++ PlataformaWeb/src/java/oc/logic/Logic.java
@@ -0,0 +1,33 @@
+package bal.hotelwebapp.logic;
+
+import bal.hotelwebapp.database.DatabaseX;
+
+public class Logic 
+{
+    private DatabaseX m_CDatabase;
+
+    public Logic() 
+    {
+        setDatabase( createDatabase() );
+    }
+
+    public DatabaseX getDatabase() 
+    {
+        return m_CDatabase;
+    }
+
+    private void setDatabase(DatabaseX p_CDatabase) 
+    {
+        m_CDatabase = p_CDatabase;
+    }
+
+    private DatabaseX createDatabase() 
+    {
+        DatabaseX database = getDatabase();
+        if(database == null)
+        {
+            database = new DatabaseX();
+        }
+        return database;
+    }    
+}
