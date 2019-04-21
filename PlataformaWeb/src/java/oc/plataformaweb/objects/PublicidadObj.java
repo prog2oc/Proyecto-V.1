b3e6ba80bd55f9544596d4a49dc46a41d4d6524d
Dera98 <David_eliceo98@hotmail.com>
2019-04-20 20:00:26 -0600

    Update PublicidadObj.java

--- PlataformaWeb/src/java/oc/plataformaweb/objects/PublicidadObj.java
+++ PlataformaWeb/src/java/oc/plataformaweb/objects/PublicidadObj.java
@@ -1,5 +1,43 @@
 package oc.plataformaweb.objects;
 
 public class PublicidadObj {
-    
+       private int m_iId;
+       private String m_DestinoDeImagen;
+       private int m_costo;
+
+    public PublicidadObj(int p_iId, String p_DestinoDeImagen, int p_costo) {
+        setId(p_iId);
+        setDestinoDeImagen(p_DestinoDeImagen);
+        setcosto(p_costo);
+    }
+
+       
+       
+       
+       
+    public int getId() {
+        return m_iId;
+    }
+
+    private void setId(int p_iId) {
+        m_iId = p_iId;
+    }
+
+    public String getDestinoDeImagen() {
+        return m_DestinoDeImagen;
+    }
+
+    private void setDestinoDeImagen(String p_DestinoDeImagen) {
+        m_DestinoDeImagen = p_DestinoDeImagen;
+    }
+
+    public int getcosto() {
+        return m_costo;
+    }
+
+    private void setcosto(int p_costo) {
+        m_costo = m_costo;
+    }
+       
+       
 }
